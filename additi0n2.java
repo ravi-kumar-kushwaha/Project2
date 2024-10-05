1741608556a47ed33469617a85da3f2ad8a1cdc9
Ravi Kumar Kushwaha <163456484+ravi-kumar-kushwaha@users.noreply.github.com>
2024-10-05 14:16:34 +0000

    initial Changes

---
+++ additi0n2.java
@@ -0,0 +1,9 @@
+public class additi0n2 {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b = 15;
+        int sum = a+b;
+        System.out.println(sum);//25
+    }
+}
